c12d3d7259878fa134b7b322273dc9d98cc191ac
soberkd <soberkd6@gmail.com>
2022-11-08 06:42:37 +0000

    Main project entry point

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function of the voting system 
+ *
+ * Return: always 0
+ */ 
+int main(void)
+{
+	int verify_reg;
+	printf("\n***************Welcome to ECK 2022***************\n");
+	printf("**Your trusted partner in elections managemant**\n");
+	printf("Kindly verify your voting status to continue\n\n");
+	printf("Enter 1 to verify your status or 2 to register as a voter\n");
+	scanf("%d", &verify_reg);
+
+	printf("\n\n\n***************Transparent***************\n");
+	return (0);
+}
