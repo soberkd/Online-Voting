d91ebae6f149ac331b7e01c083a124c815926b6a
soberkd <soberkd6@gmail.com>
2022-11-19 09:07:54 +0000

    registration page

---
+++ registration.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void registration()
+{
+	int type;
+
+	system("clear");
+	printf("Welcome to ECK registration");
+	printf("voter");
+	printf("candidate");
+	scanf("%d", &type);
+}
