f9dec56199444e9e8685b513d71b1f8f17409d28
soberkd <soberkd6@gmail.com>
2022-11-19 09:08:49 +0000

    main entry page

--- main.c
+++ main.c
@@ -8,12 +8,29 @@
 int main(void)
 {
 	int verify_reg;
-	printf("\n***************Welcome to ECK 2022***************\n");
-	printf("**Your trusted partner in elections managemant**\n");
-	printf("Kindly verify your voting status to continue\n\n");
-	printf("Enter 1 to verify your status or 2 to register as a voter\n");
+	system("clear");
+	printf("-----------------------------------------------------------");
+	printf("\n|\n\t***************Welcome to ECK 2022***************\n");
+	printf("\t**Your trusted partner in elections managemant**\n\n\n");
+	printf("-----------------------------------------------------------");
+	printf("\n\nKindly verify your voting status to continue\n\n");
+	printf("Kindly press\n1 to go to the main ECK page or\n2 to register as a voter\n");
 	scanf("%d", &verify_reg);
+	switch (verify_reg)
+	{
+		case 1:
+			main_page();
+			break;
+		case 2:
+			registration();
+			break;
+		default:
+			printf("Kindly enter a valid input");
+			break;
+	}
+
 
 	printf("\n\n\n***************Transparent***************\n");
+
 	return (0);
 }
