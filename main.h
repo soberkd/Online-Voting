8e2ae98e92dce5ec547f4474064a8008c49e5972
soberkd <soberkd6@gmail.com>
2022-11-20 18:59:05 +0000

    update main header

--- main.h
+++ main.h
@@ -3,5 +3,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+struct Login
+{
+	char fname[30];
+	char lname[30];
+	char username[30];
+	char password[20];
+};
+typedef struct Login Login;
+
+void reg_log();
+void main_page();
+void registration();
+void login();
+void main_header();
+void staff(void);
+void candidate(void);
+void voter(void);
+void admin_header();
 
 #endif
