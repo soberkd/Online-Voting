4cc45ee564b016b779e523856c8edb3bae288e55
soberkd <soberkd6@gmail.com>
2022-11-20 19:02:43 +0000

    admin panel header

---
+++ header.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * main_header - header of the main page
+ */
+void main_header()
+{
+	system("clear");
+	printf("\n\n\t***************Welcome to IEBC 2027***************\n");
+	printf("\t\t\tOnline Voting System\n");
+	printf("\t**Your trusted partner in elections managemant**\n\n");
+	printf("\t\t::::::::::::MAIN PANEL:::::::::::::\n");
+	printf("=====================================================================\n");
+}
+
+/**
+ * admin header - header of the admin panel
+ */
+void admin_header()
+{
+	printf("MAIN PANEL");
+	system("clear");
+	printf("\n\n\t***************Welcome to IEBC 2027***************\n");
+	printf("\t\t\tOnline Voting System\n");
+	printf("\t**Your trusted partner in elections managemant**\n\n");
+	printf("\t\t:::::::::::: ADMIN PANEL:::::::::::::\n");
+	printf("=====================================================================\n");
+}
+
+/**
+ * voter_header - header of the voter panel 
+ */
+void voter_header()
+{
+	printf("MAIN PANEL");
+	system("clear");
+	printf("\n\n\t***************Welcome to IEBC 2027***************\n");
+	printf("\t\t\tOnline Voting System\n");
+	printf("\t**Your trusted partner in elections managemant**\n\n");
+	printf("\t\t:::::::::::: VOTERS' PANEL:::::::::::::\n");
+	printf("=====================================================================\n");
+}
