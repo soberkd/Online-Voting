a8a927b676b7303a5a93dbad7e2e396a28bd16c3
soberkd <soberkd6@gmail.com>
2022-11-20 19:01:01 +0000

    voters page

---
+++ voter.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void voter(void)
+{
+	main_header();
+	printf("\tMake a selection to continue:\n\n");
+	 printf("\t1. Cast a vote\n\t2. Verify voter register\n\t3. View candidates\n\t4. View results\n\t5. Apply to be voting official\n\t0. Exit");
+}
